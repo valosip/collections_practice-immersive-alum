d42f794631e45998c1ccc0b54a10c81b44aa0960
Val Osip <valosip@gmail.com>
2020-04-07 04:13:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,14 @@ def sort_array_desc(array)
     y <=> x
   end
 end
+
+def sort_array_char_count(array)
+  array.sort do |x, y|
+    x.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
